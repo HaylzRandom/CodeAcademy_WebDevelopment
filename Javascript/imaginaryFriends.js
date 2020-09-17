7304d257ba8b1ab1d59493c281a3b4241bf6bc84
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2020-09-18 00:11:25 +0100

    Added HTML/CSS and Javascript challenges and lessons

---
+++ Javascript/imaginaryFriends.js
@@ -0,0 +1,10 @@
+// Write your function here:
+function numImaginaryFriends(numOfFriends) {
+    return Math.round(numOfFriends * 0.33);
+  }
+  
+  // Uncomment the line below when you're ready to try out your function
+   console.log(numImaginaryFriends(18)) // Should print 6
+  
+  // We encourage you to add more function calls of your own to test your code!
+  
---
+++ Javascript/tipCalculator.js
@@ -0,0 +1,20 @@
+// Write your function here:
+const tipCalculator = (quality, total) => {
+    switch (quality) {
+      case 'bad':
+        return (total * 0.05);
+      case 'ok':
+        return (total * 0.15);
+      case 'good':
+        return (total * 0.20);
+      case 'excellent':
+        return (total * 0.30);
+      default:
+        return (total * 0.18);
+    }
+  }
+
+  // Uncomment the line below when you're ready to try out your function
+  console.log(tipCalculator('good', 100)) //should return 20
+  console.log(tipCalculator('bad', 50));
+  // We encourage you to add more function calls of your own to test your code!
